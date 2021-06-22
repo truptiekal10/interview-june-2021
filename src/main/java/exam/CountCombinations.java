046d232814780367e139b1ed97555004fcb6b8d3
truptiekal10 <37267218+truptiekal10@users.noreply.github.com>
2021-06-22 14:24:07 +0530

    first commit

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -4,7 +4,27 @@ public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
+    int count[] = new int[N + 1];
+         
+        count[0] = 1;
+         
+        for (int i = 1; i <= N; i++)
+            for (int j = 0; j < arr.length; j++)
+     
+                if (i >= arr[j])
+                    count[i] += count[i - arr[j]];
+         
+        
+        return count[N];
+    
+    
+   
   }
+  public static void main(String[] args)
+    {
+        int N = 7;
+        System.out.println("Total number of ways = "
+                                    + countWays(N));
+    }
 
 }
